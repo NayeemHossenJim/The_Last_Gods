01add0ee3638dc012b928ba8a1aef33db9dbfb9d
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-21 17:35:54 +0600

    Add Character Input All

---
+++ Source/Slash/Private/Characters/SlashCharacter.cpp
@@ -0,0 +1,75 @@
+#include "Characters/SlashCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ASlashCharacter::ASlashCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	Cameraboom = CreateDefaultSubobject<USpringArmComponent>(TEXT("Cameraboom"));
+	Cameraboom->SetupAttachment(GetRootComponent());
+	Cameraboom->TargetArmLength = 300.f;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(Cameraboom);
+}
+
+void ASlashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ASlashCharacter::MoveForward(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		const FRotator ControlRotation = GetControlRotation();
+		const FRotator ControlRotationYaw(0, ControlRotation.Yaw, 0);
+		const FVector Forward = FRotationMatrix(ControlRotationYaw).GetUnitAxis(EAxis::X);
+		AddMovementInput(Forward, Value);
+	}
+}
+
+void ASlashCharacter::MoveRight(float Value)
+{
+	if (Controller && (Value != 0.f))
+	{
+		const FRotator ControlRotation = GetControlRotation();
+		const FRotator ControlRotationYaw(0, ControlRotation.Yaw, 0);
+		const FVector Right = FRotationMatrix(ControlRotationYaw).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Right, Value);
+	}
+}
+
+void ASlashCharacter::Turn(float Value)
+{
+	AddControllerYawInput(Value);
+}
+
+void ASlashCharacter::LookUp(float Value)
+{
+	AddControllerPitchInput(Value);
+}
+
+void ASlashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	PlayerInputComponent->BindAxis(FName("MoveForward"), this, &ASlashCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(FName("MoveRight"), this, &ASlashCharacter::MoveRight);
+	PlayerInputComponent->BindAxis(FName("Turn"), this, &ASlashCharacter::Turn);
+	PlayerInputComponent->BindAxis(FName("LookUp"), this, &ASlashCharacter::LookUp);
+}
