a07a4bdb1eef3edf0bba0ca6da941e6b38614f9d
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-21 15:30:50 +0600

    Added Pawn

---
+++ Source/Slash/Public/Pawns/Bird.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Bird.generated.h"
+
+UCLASS()
+class SLASH_API ABird : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	ABird();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+protected:
+	virtual void BeginPlay() override;
+};
