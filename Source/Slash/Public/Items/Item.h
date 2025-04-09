52c2016ba25947913c86d5d7fcecb1161b1a29c0
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-04-09 11:10:31 +0600

    Complete Niagara Section

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -48,6 +48,9 @@ protected:
 
 	UPROPERTY(VisibleAnywhere)
 	USphereComponent* Sphere;
+
+	UPROPERTY(EditAnywhere)
+	class UNiagaraComponent* EmbersEffect;
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
