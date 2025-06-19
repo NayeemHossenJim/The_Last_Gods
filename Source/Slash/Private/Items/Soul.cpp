005a8edf880f2e2bbb7773b2e37d94671162dd2a
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-06-20 00:48:53 +0600

    Soul class

---
+++ Source/Slash/Private/Items/Soul.cpp
@@ -0,0 +1,5 @@
+#include "Items/Soul.h"
+
+void ASoul::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -50,7 +50,7 @@ protected:
 	USphereComponent* Sphere;
 
 	UPROPERTY(EditAnywhere)
-	class UNiagaraComponent* EmbersEffect;
+	class UNiagaraComponent* ItemEffect;
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
